93fcf8ccca2b4a2cffe951b35a3de56f071eda0e
Ilham Daulah <ilhamdaulah9@gmail.com>
2023-04-03 01:19:02 +0700

    update darkmode + perbaikan

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,17 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const top = document.querySelector('#top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        top.classList.remove('hidden');
+        top.classList.add('flex');
+        
     } else {
         header.classList.remove('navbar-fixed');
+        top.classList.remove('flex');
+        top.classList.add('hidden');
     }
 };
 
@@ -18,3 +24,34 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+
+//click diluar hamburger
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu){
+        hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+    }
+});
+
+//darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if (darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+
+//pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+      darkToggle.checked = false;   
+  }
